e0f63a4900271be8575ab17e0ddc0640497c00e7
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-23 16:10:58 -0700

    implicit return, migrations table

--- db/migrations/20200423160540_users.js
+++ db/migrations/20200423160540_users.js
@@ -1,11 +1,8 @@
-exports.up = function (knex) {
+exports.up = (knex) =>
   knex.schema.createTable('users', (column) => {
     column.increments();
     column.string('username', 128).notNullable().unique();
     column.string('password', 128).notNullable();
   });
-};
 
-exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('users');
-};
+exports.down = (knex) => knex.schema.dropTableIfExists('users');
