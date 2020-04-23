4ed6d34111a1b108cb41f60dbbb2457a8e0dcfcc
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-23 15:07:10 -0700

    add static GET users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Users = require('../models/Users');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try {
+    const getUsers = await Users.find();
+
+    if (!getUsers) res.status(404).json(`No users found.`);
+    else res.status(200).json({ getUsers });
+  } catch (error) {
+    res.status(500).json(`Not implemented.`);
+  }
 });
 
 module.exports = router;
