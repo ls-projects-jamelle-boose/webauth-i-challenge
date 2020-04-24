c3bc5a2339ed4dfb3e40863e0ef4f4b8f5a76cfa
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-24 08:10:01 -0700

    add route logic

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+var bcrypt = require('bcrypt');
 var express = require('express');
 var router = express.Router();
 var Users = require('../models/Users');
@@ -6,11 +7,24 @@ var Users = require('../models/Users');
 router.get('/', async (req, res, next) => {
   try {
     const getUsers = await Users.find();
-
     if (!getUsers) res.status(404).json(`No users found.`);
-    else res.status(200).json({ getUsers });
+    return res.status(200).json({ Users: getUsers });
   } catch (error) {
-    res.status(500).json(`Not implemented.`);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+/* POST new user */
+router.post('/register', async (req, res) => {
+  var body = req.body;
+  var hash = bcrypt.hashSync(body.password, 8);
+  body.password = hash;
+
+  try {
+    const addUser = await Users.add(body);
+    res.status(201).json({ addUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
