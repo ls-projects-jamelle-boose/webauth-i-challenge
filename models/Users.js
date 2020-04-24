f4317e6b2e9d0a0e40ec7c60415e61f59c567622
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-24 07:53:10 -0700

    add user models

---
+++ models/Users.js
@@ -0,0 +1,10 @@
+const db = require('../db/dbConfig');
+
+exports.add = async (body) => {
+  const [id] = await db('users').insert(body);
+  return await this.findById(id);
+};
+
+exports.find = () => db('users');
+
+exports.findById = (id) => db('users').where({ id });
